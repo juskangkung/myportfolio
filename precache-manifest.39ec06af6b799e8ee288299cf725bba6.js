b966ea045d6fe01511f5effddf8bab28aa775468
juskangkung <53251131+juskangkung@users.noreply.github.com>
2020-04-21 18:04:26 +0700

    Updates

---
+++ precache-manifest.39ec06af6b799e8ee288299cf725bba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143792ed076f27dc6e6119c3295c9355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9608825e87c3c7ddf34a",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "ae16cc2aefd384982f01",
+    "url": "/static/js/2.0338e0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0338e0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9608825e87c3c7ddf34a",
+    "url": "/static/js/main.f8301e62.chunk.js"
+  },
+  {
+    "revision": "d03cb11163a540e10626",
+    "url": "/static/js/runtime-main.fb39e7d1.js"
+  }
+]);
